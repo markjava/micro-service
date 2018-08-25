211d59f8d8e8416f1d3584bc91683f7d0bdac2f1
mark_java_xy <mark_java_xy@126.com>
2018-08-25 11:07:06 +0800

    filter orther request

---
+++ micro-service-registry/src/main/java/com/micro/service/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.micro.service.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 过滤非法请求
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
